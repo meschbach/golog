bbec7e96c6d941d2cc86fbb80f186fe751ba6302
Mark Eschbach <meschbach@gmail.com>
2022-01-07 18:51:52 -0800

    term/golang_int: Ensure does not unify with atom fo string

--- term/golang_int_test.go
+++ term/golang_int_test.go
@@ -1,8 +1,12 @@
 package term
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGolangIntTerm_Unify_WithGolangIntTerm(t *testing.T) {
+	t.Parallel()
 	t1 := WrapInt(1)
 	t2 := WrapInt(1)
 
@@ -10,5 +14,18 @@ func TestGolangIntTerm_Unify_WithGolangIntTerm(t *testing.T) {
 	_, err := t1.Unify(bindings,t2)
 	if err == CantUnify {
 		t.Errorf("Expected unification of %s to %s....did not", t1, t2)
+	}
+}
+
+func TestGolangIntTerm_Unify_Atom(t *testing.T) {
+	t.Parallel()
+	t1 := WrapInt(314)
+	t2 := NewAtom("314")
+
+	fmt.Printf("T2 string: %q\n",t2)
+	bindings := NewBindings()
+	_, err := t1.Unify(bindings,t2)
+	if err != CantUnify {
+		t.Errorf("Expected unification to fail of %s to %s....did not", t1, t2)
 	}
 }
